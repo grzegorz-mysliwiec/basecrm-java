34f3af0e41930d260dc07202c8e34e692efe1f46
gmysliwiec <grzegorz.mysliwiec@getbase.com>
2016-06-24 09:29:03 +0200

    Reverted formatting

--- src/test/groovy/com/getbase/services/BaseSpecification.groovy
+++ src/test/groovy/com/getbase/services/BaseSpecification.groovy
@@ -6,7 +6,7 @@ import com.getbase.Client
 import com.getbase.Configuration
 import spock.lang.Specification
 
-abstract class BaseSpecification extends Specification {
+abstract class BaseSpecification  extends Specification {
     def getAccessToken() {
         def token = System.getenv("BASECRM_ACCESS_TOKEN")
         if (token == null || token.isEmpty())
@@ -42,7 +42,7 @@ abstract class BaseSpecification extends Specification {
     def createAssociatedContact(attributes = [:]) {
         def dealId = createDeal().id;
         def associatedContactAttributes = [
-                'role'      : "involved",
+                'role': "involved",
                 'contact_id': createContact().id,
         ]
         associatedContactAttributes << attributes
@@ -54,23 +54,23 @@ abstract class BaseSpecification extends Specification {
 
     def createContact(attributes = [:]) {
         def contactAttributes = [
-                'description'    : "I know him via Tom",
-                'email'          : "mark@designservices.com",
-                'facebook'       : "mjohnson",
-                'fax'            : "+44-208-1234567",
-                'first_name'     : 'Mark' + nextRand(),
-                'industry'       : "Design Services",
+                'description': "I know him via Tom",
+                'email': "mark@designservices.com",
+                'facebook': "mjohnson",
+                'fax': "+44-208-1234567",
+                'first_name': 'Mark' +  nextRand(),
+                'industry': "Design Services",
                 'is_organization': false,
-                'last_name'      : 'Johnson' + nextRand(),
-                'linkedin'       : "mjohnson",
-                'mobile'         : "508-778-6516",
-                'name'           : 'Design Services Company' + nextRand(),
-                'phone'          : "508-778-6516",
-                'skype'          : "mjohnson",
-                'title'          : "CEO",
-                'twitter'        : "mjohnson",
-                'website'        : "www.designservices.com",
-                'tags'           : ["important"],
+                'last_name': 'Johnson' +  nextRand(),
+                'linkedin': "mjohnson",
+                'mobile': "508-778-6516",
+                'name': 'Design Services Company' +  nextRand(),
+                'phone': "508-778-6516",
+                'skype': "mjohnson",
+                'title': "CEO",
+                'twitter': "mjohnson",
+                'website': "www.designservices.com",
+                'tags': ["important"],
         ]
         contactAttributes << attributes
         def contact = client.contacts().create(contactAttributes);
@@ -80,11 +80,11 @@ abstract class BaseSpecification extends Specification {
 
     def createDeal(attributes = [:]) {
         def dealAttributes = [
-                'currency'  : "EUR",
-                'hot'       : true,
-                'name'      : 'Website Redesign' + nextRand(),
-                'value'     : 1000.0,
-                'tags'      : ["important"],
+                'currency': "EUR",
+                'hot': true,
+                'name': 'Website Redesign' +  nextRand(),
+                'value': 1000.0,
+                'tags': ["important"],
                 'contact_id': createContact().id,
         ]
         dealAttributes << attributes
@@ -95,11 +95,11 @@ abstract class BaseSpecification extends Specification {
 
     def createDecimalDeal(attributes = [:]) {
         def dealAttributes = [
-                'currency'  : "EUR",
-                'hot'       : true,
-                'name'      : 'Website Redesign' + nextRand(),
-                'value'     : '1000.25',
-                'tags'      : ["important"],
+                'currency': "EUR",
+                'hot': true,
+                'name': 'Website Redesign' +  nextRand(),
+                'value': '1010.25',
+                'tags': ["important"],
                 'contact_id': createContact().id,
         ]
         dealAttributes << attributes
@@ -111,21 +111,21 @@ abstract class BaseSpecification extends Specification {
     def createLead(attributes = [:]) {
         def leadAttributes = [
                 'description': "I know him via Tom",
-                'email'      : "mark@designservices.com",
-                'facebook'   : "mjohnson",
-                'fax'        : "+44-208-1234567",
-                'first_name' : 'Mark' + nextRand(),
-                'industry'   : "Design Services",
-                'last_name'  : 'Johnson' + nextRand(),
-                'linkedin'   : "mjohnson",
-                'mobile'     : "508-778-6516",
-                'phone'      : "508-778-6516",
-                'skype'      : "mjohnson",
-                'status'     : "Unqualified",
-                'title'      : "CEO",
-                'twitter'    : "mjohnson",
-                'website'    : "www.designservices.com",
-                'tags'       : ["important"],
+                'email': "mark@designservices.com",
+                'facebook': "mjohnson",
+                'fax': "+44-208-1234567",
+                'first_name': 'Mark' +  nextRand(),
+                'industry': "Design Services",
+                'last_name': 'Johnson' +  nextRand(),
+                'linkedin': "mjohnson",
+                'mobile': "508-778-6516",
+                'phone': "508-778-6516",
+                'skype': "mjohnson",
+                'status': "Unqualified",
+                'title': "CEO",
+                'twitter': "mjohnson",
+                'website': "www.designservices.com",
+                'tags': ["important"],
         ]
         leadAttributes << attributes
         def lead = client.leads().create(leadAttributes);
@@ -135,7 +135,7 @@ abstract class BaseSpecification extends Specification {
 
     def createLossReason(attributes = [:]) {
         def lossReasonAttributes = [
-                'name': 'We were too expensive' + nextRand(),
+                'name': 'We were too expensive' +  nextRand(),
         ]
         lossReasonAttributes << attributes
         def lossReason = client.lossReasons().create(lossReasonAttributes);
@@ -145,8 +145,8 @@ abstract class BaseSpecification extends Specification {
 
     def createNote(attributes = [:]) {
         def noteAttributes = [
-                'content'      : "Highly important.",
-                'resource_id'  : createContact().id,
+                'content': "Highly important.",
+                'resource_id': createContact().id,
                 'resource_type': 'contact',
         ]
         noteAttributes << attributes
@@ -157,7 +157,7 @@ abstract class BaseSpecification extends Specification {
 
     def createSource(attributes = [:]) {
         def sourceAttributes = [
-                'name': 'Word of mouth' + nextRand(),
+                'name': 'Word of mouth' +  nextRand(),
         ]
         sourceAttributes << attributes
         def source = client.sources().create(sourceAttributes);
@@ -167,7 +167,7 @@ abstract class BaseSpecification extends Specification {
 
     def createTag(attributes = [:]) {
         def tagAttributes = [
-                'name'         : 'publisher' + nextRand(),
+                'name': 'publisher' +  nextRand(),
                 'resource_type': 'contact',
         ]
         tagAttributes << attributes
@@ -178,10 +178,10 @@ abstract class BaseSpecification extends Specification {
 
     def createTask(attributes = [:]) {
         def taskAttributes = [
-                'content'      : "Contact Tom",
-                'due_date'     : "2014-09-27T16:32:56+00:00",
-                'remind_at'    : "2014-09-29T15:32:56+00:00",
-                'resource_id'  : createContact().id,
+                'content': "Contact Tom",
+                'due_date': "2014-09-27T16:32:56+00:00",
+                'remind_at': "2014-09-29T15:32:56+00:00",
+                'resource_id': createContact().id,
                 'resource_type': 'contact',
         ]
         taskAttributes << attributes
@@ -192,24 +192,24 @@ abstract class BaseSpecification extends Specification {
 
     def createProduct(attributes = [:]) {
         def productAttributes = [
-                "name"         : "Enterprise Plan",
-                "sku"          : "enterprise-plan",
-                "description"  : "Includes more storage options",
-                "active"       : true,
-                "prices"       : [
+                "name": "Enterprise Plan",
+                "sku": "enterprise-plan",
+                "description": "Includes more storage options",
+                "active": true,
+                "prices": [
                         [
-                                "amount"  : "1599.99",
+                                "amount": "1599.99",
                                 "currency": "USD"
                         ],
                         [
-                                "amount"  : "3599.99",
+                                "amount": "3599.99",
                                 "currency": "PLN"
                         ]
                 ],
-                "cost"         : "999.99",
+                "cost": "999.99",
                 "cost_currency": "USD",
-                "max_discount" : 15,
-                "max_markup"   : 5,
+                "max_discount": 15,
+                "max_markup": 5,
         ]
         productAttributes << attributes
         def product = client.products().create(productAttributes);
